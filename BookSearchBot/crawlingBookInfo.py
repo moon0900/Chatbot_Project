1c629cedb8f0088894fa4ed4e1a3686f1016977e
moon0900 <73999954+moon0900@users.noreply.github.com>
2022-10-11 02:45:13 +0900

    Update crawlingBookInfo.py

--- BookSearchBot/crawlingBookInfo.py
+++ BookSearchBot/crawlingBookInfo.py
@@ -1,26 +1,31 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('../chromedriver.exe')
 driver.get('https://discover.duksung.ac.kr/#/')
 
 def getBookInfo(i):
     elem = driver.find_element_by_css_selector('div.ikc-search-item:nth-child('+str(i)+') > div.ikc-item-info > ul > li:nth-child(1) > a')
     elem.click()
-    time.sleep(0.5)
+    elem = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.ikc-biblio-info > p > span')))
+    bookType = elem.text
     bookInfo = driver.find_element_by_css_selector('div.ikc-biblio-info > ul')
     bookTitle = bookInfo.find_element_by_css_selector('li:nth-child(1) > span > a').text.split('/')
     bookPub = bookInfo.find_element_by_css_selector('li:nth-child(2) > span > span').text.split(',')
     result = {
+        '자료유형': bookType,
         '도서명': bookTitle[0].strip(),
-        '저자': bookTitle[1].strip(),
         '발행처': bookPub[0].split(':')[1].strip(),
         '발행년도': bookPub[1].strip()
     }
-
+    if len(bookTitle) > 1:
+        result['저자'] = bookTitle[1].strip()
     locList = []
     bookInfo = driver.find_elements_by_css_selector('tbody.ikc-item')
     for info in bookInfo:
@@ -62,38 +67,38 @@ def startSearch(keyword):
     elem = driver.find_element_by_class_name('ikc-btn-search')
     elem.click()
 
-    time.sleep(0.5)
     result = getSearchResult()
 
     return result
 
 def getSearchResult():
-    elem = driver.find_elements_by_css_selector('div.ikc-search-result-listup > p')
-    if len(elem) == 0:  # 검색 결과 없음
-        return 0
-    else:
-        elem = elem[0]  # 검색 결과 있음
+    noresult, result = '', ''
+    while(len(noresult)==0 and len(result)==0):
+        noresult = driver.find_elements_by_css_selector('div.ikc-search-noresult.ikc-no-record > strong')
+        if len(noresult) > 0:
+            noresult = noresult[0].text
+        result = driver.find_elements_by_css_selector('div.ikc-search-result-listup > p')
+        if len(result) > 0:
+            result = result[0].text
 
-    cntText = elem.text.split()
+    if len(result) == 0:  # 검색 결과 없음
+        return 0
+
+    cntText = result.split()
     cntText = cntText[2].strip()  # 검색 결과 수
 
     if cntText == '1':
         return 1, getBookInfo(1)
+    elif int(cntText.replace(',', '')) < 6:
+        return 2, cntText, getTopItems()
     else:
-        str = f'총 {cntText} 건의 검색 결과가 존재합니다.'
-        if int(cntText.replace(',', '')) < 6:
-            str += '\n검색된 결과는 다음과 같습니다.'
-            return 2, str, getTopItems()
-        else:
-            str += '\n상위 5개의 검색 결과는 다음과 같습니다.'
-            return 3, str, getTopItems()
+        return 3, cntText, getTopItems()
 
 def addKeywordSearch(option, input):
     detail_btn = driver.find_element_by_css_selector('div.ikc-search-topbtns > a')
     options = ('서명', '저자', '발행처')
     if detail_btn.text != '간략검색':
         detail_btn.click()
-        time.sleep(0.5)
     if option < 4:
         ex_keyword = driver.find_elements_by_css_selector(
             'div.ikc-search-ex-keyword-wrap.col-lg-6.col-md-6 > div.ikc-search-ex-keyword:not(.ng-hide)')[-1]
@@ -109,6 +114,5 @@ def addKeywordSearch(option, input):
         elem.send_keys(input)
     driver.find_element_by_css_selector('div.ikc-search-ex-btns > input.ikc-btn-search').click()
     time.sleep(0.5)
-
     result = getSearchResult()
     return result
